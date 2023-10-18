13edf5639ee37724aeaabcbeb6def4037545b4ab
adam kiryk <akiryk@wayfair.com>
2023-10-18 15:00:55 -0400

    able to query on client

--- app/api/graphql/resolvers.js
+++ app/api/graphql/resolvers.js
@@ -4,7 +4,10 @@ export const resolvers = {
   Query: {
     hello: () => "GraphQL is Awesome",
     welcome: (_, params) => `Hello ${params.name}`,
-    students: async () => await Student.find({}),
+    students: async (parent, args, context) => {
+      console.log("asking for students", parent, args, context);
+      return await Student.find({});
+    },
     student: async (parent, args) => await Student.findById(args.id),
   },
   Mutation: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,47 @@
+import { gql } from "@apollo/client";
+import { getClient } from "../lib/apolloClient";
+
+type Student = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  id: string;
+};
+
+const GET_STUDENTS = gql`
+  query getStudents {
+    students {
+      firstName
+      lastName
+      age
+      id
+    }
+  }
+`;
+
 export default async function Home() {
+  const { data, error, loading } = await getClient().query({
+    query: GET_STUDENTS,
+  });
+
+  if (loading) {
+    return <p>...loading</p>;
+  }
+
+  if (error) {
+    return null;
+  }
+
   return (
     <div className="container">
       <main>
-        <p>This is what it is</p>
+        <ul>
+          {data.students.map((student: Student) => (
+            <li>
+              {student.firstName} {student.lastName}: {student.id}
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
